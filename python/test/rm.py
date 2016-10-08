81d51743af2e02225ce99018d269f5061be0acc0
zhouyifan <552094467@qq.com>
2016-10-08 13:31:11 +0800

    modify rm.py

--- python/test/rm.py
+++ python/test/rm.py
@@ -7,7 +7,7 @@ import shutil
 from collections import OrderedDict
 
 
-def rm_dir(path, rm_list):
+def rm_dir(path, rm_list, ignore_list):
     def _rm_dir(path):
         os.chdir(path)
 
@@ -18,26 +18,30 @@ def rm_dir(path, rm_list):
             if os.path.isdir(item):
                 item += '/'
 
-            for pattern in rm_list:
+            for pattern in ignore_list:
                 if re.match(pattern, item):
-                    log('del {0}'.format(os.path.abspath(item)))
-
-                    if os.path.isdir(item):
-                        shutil.rmtree(item, ignore_errors=True)
-                    else:
-                        os.remove(item)
                     break
             else:
-                log('    {0}'.format(os.path.abspath(item)))
-                if os.path.isdir(item):
-                    _rm_dir(os.path.abspath(item))
-                    os.chdir(path)
+                for pattern in rm_list:
+                    if re.match(pattern, item):
+                        log('del {0}'.format(os.path.abspath(item)))
+
+                        if os.path.isdir(item):
+                            shutil.rmtree(item, ignore_errors=True)
+                        else:
+                            os.remove(item)
+                        break
+                else:
+                    log('    {0}'.format(os.path.abspath(item)))
+                    if os.path.isdir(item):
+                        _rm_dir(os.path.abspath(item))
+                        os.chdir(path)
 
     _rm_dir(path)
 
 
 def init_log(show=True):
-    log_path = '/Users/zhouyifan/Code/test-code/python/test/log.txt'
+    log_path = os.path.abspath('.') + '/log.txt'
     if os.path.exists(log_path):
         os.remove(log_path)
 
@@ -57,7 +61,7 @@ if __name__ == '__main__':
     else:
         path = sys.argv[1]
 
-    path = os.path.join(os.path.curdir, path)
+    path = os.path.abspath(path)
 
     def replace(old_str):
         pattern = OrderedDict([
@@ -72,7 +76,16 @@ if __name__ == '__main__':
 
         return '^{0}$'.format(old_str)
 
+    rm_list = []
+    ignore_list = []
     with open('.rm-list') as f:
-        rm_list = list(
-            map(replace, filter(lambda item: item.replace('\n', ''), f)))
-    rm_dir(path, rm_list)
+        for line in f:
+            item = line.replace('\n', '')
+            if not item:
+                continue
+
+            if item.startswith('-'):
+                ignore_list.append(replace(item[1:]))
+            else:
+                rm_list.append(replace(item))
+    rm_dir(path, rm_list, ignore_list)
