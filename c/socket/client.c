482f3e7a6e9d88a39f80e736b031ea258a24f44b
zhouyifan <552094467@qq.com>
2016-10-10 22:08:50 +0800

    modify socket

--- c/socket/client.c
+++ c/socket/client.c
@@ -3,14 +3,49 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+
+#define BUFF_LEN 4096
 
 int main(int argc, char const *argv[]) {
-	const int BUFF_LEN = 4096;
-    int clientfd = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in serv_addr;
-    struct hostent * server = gethostbyname(argv[1]);
-    char buff[BUFF_LEN];
-    int portno = atoi(argv[2]);
+	char buff[BUFF_LEN];
+	if (argc < 3) {
+		fprintf(stderr,"usage %s hostname port\n", argv[0]);
+		exit(0);
+	}
+	struct hostent * server = gethostbyname(argv[1]);
+	if(server == NULL){
+		fprintf(stderr, "ERROR, no such host\n");
+		exit(0);
+	}
+	int portno = atoi(argv[2]);
 
-	return 0;
+    int clientfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (clientfd < 0){
+		fprintf(stderr, "ERROR opening socket\n");
+		exit(1);
+	}
+    struct sockaddr_in serv_addr;
+
+    bzero((char*)&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(portno);
+
+	if(connect(clientfd, (const struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
+		fprintf(stderr, "ERROR connecting\n");
+		exit(1);
+	}
+
+	printf("Please enter the message: ");
+	bzero(buff, BUFF_LEN);
+	fgets(buff, BUFF_LEN, stdin);
+	int n = write(clientfd, buff, BUFF_LEN);
+	if(n < 0){
+		fprintf(stderr, "ERROR writing to socket\n");
+	}
+
+    return 0;
 }
--- c/socket/server.c
+++ c/socket/server.c
@@ -4,11 +4,24 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <unistd.h>
+#include <error.h>
+
+#define  BUFF_LEN 4096
 
 int main(int argc, char const *argv[]) {
-	const int BUFF_LEN = 4096;
 	char buff[BUFF_LEN];
+
+	if(argc < 2){
+		fprintf(stderr, "no port provided\n");
+		exit(1);
+	}
+
 	int serverfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(serverfd < 0){
+		fprintf(stderr, "ERROR opening socket\n");
+		exit(1);
+	}
 
 	struct sockaddr_in serv_addr;
 	bzero((char*)&serv_addr, sizeof(serv_addr));
@@ -18,14 +31,25 @@ int main(int argc, char const *argv[]) {
 	serv_addr.sin_port = htons(portno);
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
 
-	bind(serverfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+	if(bind(serverfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0){
+		fprintf(stderr, "ERROR on binding\n");
+		exit(1);
+	}
 	listen(serverfd, 10);
 	while (1) {
 		bzero(buff, BUFF_LEN);
 
 		int connfd = accept(serverfd, NULL, NULL);
+		if(connfd < 0){
+			fprintf(stderr, "ERROR on accept\n");
+			exit(1);
+		}
 
-		int n = recv(connfd, buff, BUFF_LEN, 0);
+		int n = read(connfd, buff, BUFF_LEN);
+		if(n < 0){
+			fprintf(stderr, "ERROR reading from socker\n");
+			exit(1);
+		}
 		printf("%s\n", buff);
 	}
 	return 0;
